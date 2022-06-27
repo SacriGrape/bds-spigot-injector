a1f330b605a7ce347bb915f1b3b022c1e6ed4627
Evan Lee <evan6lee@gmail.com>
2022-06-27 13:19:12 -0500

    Improved Error messages and removed final printout to make output cleaner

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,12 @@ fn main() {
     println!("[Spigot Injector INFO] Spawning BDS...");
     let id = Command::new("./bedrock_server.exe")
         .spawn()
-        .expect("Failed to spawn BDS")
+        .expect("[Spigot Injector ERROR] Failed to spawn BDS")
         .id();
 
     let bds_process = Process::from_pid(id).unwrap();
     println!("[Spigot Injector INFO] BDS Spawned!!! Injecting...");
     bds_process
         .inject("./spigot_loader.dll")
-        .expect("Failed to inject DLL");
-
-    println!("[Spigot Injector] INJECTION COMPLETE!!! Injector is done!!");
+        .expect("[Spigot Injector ERROR] Failed to inject DLL");
 }
